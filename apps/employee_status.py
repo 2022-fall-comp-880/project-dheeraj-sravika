39052d26b9832e8793620e7398d442b41fb829c9
Sai Dheeraj Kistapuram <SaiDheeraj.Kistapuram@unh.edu>
2022-12-21 12:01:45 -0500

    Updated module for salary ranges

--- apps/employee_status.py
+++ apps/employee_status.py
@@ -8,10 +8,9 @@ class EmployeeStatus:
     def result(self) -> dict:
         """List of job roles with an employment status for each city."""
         output = {}
-        with open('data.csv', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row['field1'])
+        # with open('data.csv', newline='') as csvfile:
+        #   reader = csv.DictReader(csvfile)
+        for row in self.dataset:
             if row[5] not in output:
                 output[row[5]] = dict()
             if row[-2] in output[row[5]]:
